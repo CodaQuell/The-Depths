7de11304f89945ecde2bd22828734fcdc3490c25
Coda <122181428+CodaQuell@users.noreply.github.com>
2023-12-07 19:58:51 +1100

    Delete TheDepths directory

--- TheDepths/Class/NonLivingClass/ItemClass.py
+++
@@ -1,6 +0,0 @@
-#the base class for all items
-class Item():
-    def __init__(self):
-        self.name = ''
-        self.type = ''
-        self.flavour = ''
--- TheDepths/Class/NonLivingClass/PotionClass.py
+++
@@ -1,9 +0,0 @@
-from Class.NonLivingClass.ItemClass import Item
-
-class Potion(Item):
-    def __init__(self, name, flavour, potion_strength, potion_strength_count, potion_invisibility_count):
-        self.name = ''
-        self.flavour = ''
-        self.potion_strength = 0
-        self.potion_strength_count = 0
-        self.potion_invisibility_count = 0
